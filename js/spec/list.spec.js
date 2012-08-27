9078f8bd99956c5b6b137f9bbb7ac951cd66af8d
Mikael Lundin <mikael.lundin@litemedia.se>
2012-08-27 04:38:28 +0200

    Completed both fold and partition functions for js

--- js/spec/list.spec.js
+++ js/spec/list.spec.js
@@ -36,3 +36,71 @@ describe("Map", function() {
     expect(result).toEqual([]);
   });
 });
+
+describe("Fold", function() {
+
+  it("aggregates all integers to a sum", function() {
+
+    // act
+    result = [1, 2, 3].fold(0, function(agg, item) { return agg + item; });
+
+    // assert
+    expect(result).toBe(6);
+  });
+
+  it("join all numbers into a string", function() {
+
+    // act
+    result = [1, 2, 3].fold("", function(agg, item) { return agg + item.toString(); });
+
+    // assert
+    expect(result).toBe("123");
+  });
+
+
+  it("join all numbers into one number", function() {
+
+    // act
+    result = [1, 2, 3].fold(0, function(agg, item) {
+      return agg * 10 + item;
+    });
+
+    // assert
+    expect(result).toBe(123);
+  });
+});
+
+describe("Partition", function() {
+
+  it("splits odds and evens", function() {
+
+    // act
+    result = [1, 2, 3, 4, 5].partition(function (item) { return item % 2; });
+
+    // assert
+    expect(result[0]).toEqual([2, 4]);
+    expect(result[1]).toEqual([1, 3, 5]);
+  });
+
+  it("must yield a number from callback", function() {
+
+    // act
+    result = function () {
+      return [1, 2, 3].partition(function (item) { return "1"; });
+    };
+
+    // assert
+    expect(result).toThrow("callback should return a positive number");
+  });
+
+  it("must yield a positive integer from callback", function() {
+
+    // act
+    result = function () {
+      return [1, 2, 3].partition(function (item) { return -1; });
+    };
+
+    // assert
+    expect(result).toThrow("callback should return a positive number");
+  }); 
+});
--- js/src/list.js
+++ js/src/list.js
@@ -13,21 +13,82 @@ var holf = holf || {};
 holf.list = {
 
     /**
-    * Map each element of list with callback
+    * Map each element of array with callback
     *
     * @method map
-    * @param  {enumerable} list     List of elements to be transformed
-    * @param  {function}   callback Transform each element
-    * @return {enumerable} New list with each element transformed
+    * @param  {function} callback Transform each element
+    * @param  {array}    arr     Array of elements to be transformed
+    * @return {array}    New array with each element transformed
     */
-    map: function (list, callback) {
+    map: function (callback, arr) {
         "use strict";
         var result = [],
             i = 0,
             max = 0;
 
-        for (i = 0, max = list.length; i < max; i += 1) {
-            result[i] = callback(list[i]);
+        for (i = 0, max = arr.length; i < max; i += 1) {
+            result[i] = callback(arr[i]);
+        }
+
+        return result;
+    },
+
+    /**
+     * Aggregate the elements of an array
+     *
+     * @method fold
+     * @param  {function} callback Aggregation function
+     * @param  {object}   seed      Seed value of the aggregation
+     * @param  {array}    arr       Array of elements to be aggregated
+     * @return {object}   The aggregated value
+     */
+    fold: function (callback, seed, arr) {
+        "use strict";
+        var result = seed,
+            i = 0,
+            max = 0;
+
+        for (i = 0, max = arr.length; i < max; i += 1) {
+            result = callback(result, arr[i]);
+        }
+
+        return result;
+    },
+
+    /**
+     * Partitions elements of an array
+     *
+     * @method partition
+     * @param  {function} callback Partition function
+     * @param  {array}    arr      Array of elements to be partitions
+     * @return {object}   An array with the partitions
+     */
+    partition: function (callback, arr) {
+        "use strict";
+        var result = [],
+            i = 0,
+            j = 0,
+            sourceMax = 0,
+            resultMax = 0,
+            index = -1;
+        for (i = 0, sourceMax = arr.length; i < sourceMax; i += 1) {
+            index = callback(arr[i]);
+
+            // sane check of index
+            if (typeof index !== "number" || index < 0) {
+                throw "callback should return a positive number";
+            }
+
+            resultMax = result.length;
+            if (index > resultMax - 1) {
+                // expand the result
+                for (j = resultMax; j <= index; j += 1) {
+                    result.push([]);
+                }
+            }
+
+            // push item to bucket
+            result[index].push(arr[i]);
         }
 
         return result;
@@ -39,7 +100,19 @@ holf.list = {
     "use strict";
     if (typeof Array.prototype.map !== "function") {
         Array.prototype.map = function (callback) {
-            return holf.list.map(this, callback);
+            return holf.list.map(callback, this);
+        };
+    }
+
+    if (typeof Array.prototype.fold !== "function") {
+        Array.prototype.fold = function (seed, callback) {
+            return holf.list.fold(callback, seed, this);
+        };
+    }
+
+    if (typeof Array.prototype.partition !== "function") {
+        Array.prototype.partition = function (callback) {
+            return holf.list.partition(callback, this);
         };
     }
 }());
